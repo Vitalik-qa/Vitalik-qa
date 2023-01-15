9e310769aa43d24319d16dd7e59f79bc14b4a883
Vitalik <105981469+Vitalik-qa@users.noreply.github.com>
2023-01-15 12:17:35 +0300

    Update route.py

--- route.py
+++ route.py
@@ -13,6 +13,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 
+# Функция проверки наличия веб элемента
 def check(xpath):
     try:
         driver.find_element(By.XPATH, xpath)
@@ -20,9 +21,10 @@ def check(xpath):
         return False
     return True
 
+
 # Подключаем веб браузер
-s = Service('C:/Users/Виталий/Desktop/Autotest/chromedriver/chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+s = "chromedriver.exe"
+driver = webdriver.Chrome(executable_path=s)
 # Открываем сайт с Расписанием
 driver.get("https://pass.rzd.ru/")
 # Открываем окно на весь экран
@@ -50,7 +52,7 @@ from_time.send_keys(Keys.CONTROL + "a")
 from_time.send_keys(Keys.DELETE)
 from_time.send_keys(tomorrow.strftime("%d.%m.%Y"))
 # Поиск
-search = driver.find_element(By.ID,"Submit")
+search = driver.find_element(By.ID, "Submit")
 search.send_keys(Keys.ENTER)
 # Ожидание появления поездов
 train_xpath = "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div[1]"
@@ -63,33 +65,71 @@ i = 1
 # Цикл проверки наличия следующего поезда по порядку
 while check(train_xpath):
     # Изменения переменных для проверки наличия веб элемента на страницы
-    price_xpath = "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div[" + str(j) + "]/div/div/div[1]/div[2]/div[1]/div[" + str(i) + "]/div/div[3]/span[2]"
+    price_xpath = (
+        "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div["
+        + str(j)
+        + "]/div/div/div[1]/div[2]/div[1]/div["
+        + str(i)
+        + "]/div/div[3]/span[2]"
+    )
     train_xpath = "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div[" + str(j) + "]"
     # Цикл проверки цены
     while check(price_xpath):
         # Поиск минимальной цены
         print(driver.find_element(By.XPATH, price_xpath).text)
-        if  price > int((driver.find_element(By.XPATH, price_xpath).text).replace(" ", "")):
+        if price > int(
+            (driver.find_element(By.XPATH, price_xpath).text).replace(" ", "")
+        ):
             # Сохранения переменных: Названия поезда
-            name = driver.find_element(By.XPATH, "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div[" + str(j) + "]/div/div/div[1]/div[1]/div[2]/span[2]").text
+            name = driver.find_element(
+                By.XPATH,
+                "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div["
+                + str(j)
+                + "]/div/div/div[1]/div[1]/div[2]/span[2]",
+            ).text
             # Тип вагона
-            ticket = driver.find_element(By.XPATH, "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div[" + str(j) + "]/div/div/div[1]/div[2]/div[1]/div[" + str(i) + "]/div/div[2]/span").text
+            ticket = driver.find_element(
+                By.XPATH,
+                "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div["
+                + str(j)
+                + "]/div/div/div[1]/div[2]/div[1]/div["
+                + str(i)
+                + "]/div/div[2]/span",
+            ).text
             # Цена
-            price = driver.find_element(By.XPATH, "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div[" + str(j) + "]/div/div/div[1]/div[2]/div[1]/div[" + str(i) + "]/div/div[3]/span[2]").text
+            price = driver.find_element(
+                By.XPATH,
+                "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div["
+                + str(j)
+                + "]/div/div/div[1]/div[2]/div[1]/div["
+                + str(i)
+                + "]/div/div[3]/span[2]",
+            ).text
             # Убираем пробел и преобразуем в int
             price = int(price.replace(" ", ""))
         # Переходим на следующую цену
         i += 1
         # Изменяем  адрес цен на следующую
-        price_xpath = "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div["+str(j)+"]/div/div/div[1]/div[2]/div[1]/div[" + str(i) + "]/div/div[3]/span[2]"
+        price_xpath = (
+            "//*[@id='Page0']/div/div[2]/div[1]/div[3]/div["
+            + str(j)
+            + "]/div/div/div[1]/div[2]/div[1]/div["
+            + str(i)
+            + "]/div/div[3]/span[2]"
+        )
     # Изменяем и обновляем переменные для изменения адресов веб элементов
     j += 1
     i = 1
 # Вывод
-print("Минимальная цена покупки билета на сайте РЖД у ФПК:", price, "руб.", ticket, "Поезд:", name)
+print(
+    "Минимальная цена покупки билета на сайте РЖД у ФПК:",
+    price,
+    "руб.",
+    ticket,
+    "Поезд:",
+    name,
+)
 # Ожидание 5 сек.
 time.sleep(5)
 # Закрываем браузер
 driver.close()
-# Функция проверки наличия веб элемента
-
