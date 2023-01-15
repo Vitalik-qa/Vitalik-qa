19e459bb4d58322327f2796195ac9511f998378e
Vitalik <105981469+Vitalik-qa@users.noreply.github.com>
2023-01-15 12:10:20 +0300

    Update map.py

--- map.py
+++ map.py
@@ -15,17 +15,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 # Проверка наличия элемента
-
-def check (xpath):
+def check(xpath):
     try:
         driver.find_element(By.XPATH, xpath)
     except NoSuchElementException:
         return False
     return True
 
+
 # Подключаем веб браузер
-s = Service('C:/Users/Виталий/Desktop/Autotest/chromedriver/chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+s = 'chromedriver.exe'
+driver = webdriver.Chrome(executable_path=s)
 # Открываем Яндекс
 driver.get("https://yandex.ru/maps/")
 
@@ -37,7 +37,7 @@ assert "Яндекс" in driver.title
 assert "No results found." not in driver.page_source
 
 # Находим Поисковую строку
-way = driver.find_element(By.XPATH,  "//*[@class='input__control _bold']")
+way = driver.find_element(By.XPATH, "//*[@class='input__control _bold']")
 way.send_keys("Ростов-на-Дону площадь Гагарина, 1")
 way.send_keys(Keys.ENTER)
 
@@ -47,12 +47,16 @@ route = driver.find_element(By.XPATH, "//*[text()='Маршрут']")
 route.click()
 
 # Ввод места отбытия
-to_place = driver.find_element(By.XPATH, "//*[@class='input__control' and @placeholder= 'Откуда']")
+to_place = driver.find_element(
+    By.XPATH, "//*[@class='input__control' and @placeholder= 'Откуда']"
+)
 to_place.send_keys("Большая Садовая ул., 134/157")
 to_place.send_keys(Keys.ENTER)
 
 # Выбираем маршшру
-foot = driver.find_element(By.XPATH, "//*[@class='route-travel-modes-view__mode _mode_pedestrian']")
+foot = driver.find_element(
+    By.XPATH, "//*[@class='route-travel-modes-view__mode _mode_pedestrian']"
+)
 foot.click()
 
 # Переменные для проверки наличия элменета
@@ -62,15 +66,27 @@ i = 1
 # Цикл проверки наличия элмента
 while check(xpath):
     # Находим время маршрута
-    time = driver.find_element(By.XPATH, "//*[@class = 'route-list-view__list']/div[" + str(i) + "]/div[1]/div[1]/div[1]")
+    time = driver.find_element(
+        By.XPATH,
+        "//*[@class = 'route-list-view__list']/div["
+        + str(i)
+        + "]/div[1]/div[1]/div[1]",
+    )
     # Находим растония маршрута
-    distances = driver.find_element(By.XPATH, " //*[@class = 'route-list-view__list']/div[" + str(i) + "]/div[1]/div[1]/div[2]")
+    distances = driver.find_element(
+        By.XPATH,
+        " //*[@class = 'route-list-view__list']/div["
+        + str(i)
+        + "]/div[1]/div[1]/div[2]",
+    )
     # Выводим
     print("Маршрут №", i, ":", time.text, ",", distances.text)
     # Увеличиваем i на 1
     i += 1
     # Изменяем переменную xpath
-    xpath = "//*[@class = 'route-list-view__list']/div[" + str(i) + "]/div[1]/div[1]/div[1]"
+    xpath = (
+        "//*[@class = 'route-list-view__list']/div[" + str(i) + "]/div[1]/div[1]/div[1]"
+    )
 
 # Закрываем браузер
 driver.close()
