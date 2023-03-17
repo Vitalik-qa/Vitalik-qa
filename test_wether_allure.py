80fb06273dd1aaab278cca3de2785e9835c90ad1
Vitalik <105981469+Vitalik-qa@users.noreply.github.com>
2023-03-17 22:18:20 +0300

    Update test_wether_allure.py

--- test_wether_allure.py
+++ test_wether_allure.py
@@ -15,17 +15,20 @@ from allure_commons.types import AttachmentType
 
 
 class TestPageSearch:
+
     def setup(self):
-        s = "chromedriver.exe"
+        s = 'chromedriver.exe'
         self.driver = webdriver.Chrome(executable_path=s)
 
     def teardown(self):
         self.driver.quit()
 
-    @allure.feature("Open pages")
-    @allure.story("Погода")
+    @allure.feature('Open pages')
+    @allure.story('Погода')
     def test_wether(self):
-
+        """
+        Этот тест проверяет  поиск погоды
+        """
         with allure.step("Открытия сайта Яндекс"):
             driver = self.driver
             driver.get("https://ya.ru/")
@@ -38,28 +41,22 @@ class TestPageSearch:
             elem.send_keys(Keys.ENTER)
 
         with allure.step("Поиск данных погоды"):
-            weather = driver.find_element(
-                By.XPATH, "//*[@id='search-result']/li[1]/div/div[2]/div/div/div[1]"
-            )
-            condition_web = driver.find_element(
-                By.XPATH, "//*[@id='search-result']/li[1]/div/div[2]/div/div/div[2]"
-            )
+            weather = driver.find_element(By.XPATH, "//*[@id='search-result']/li[1]/div/div[2]/div/div/div[1]")
+            condition_web = driver.find_element(By.XPATH, "//*[@id='search-result']/li[1]/div/div[2]/div/div/div[2]")
 
         with allure.step("Поиск скорости ветра"):
-            flesh_web = driver.find_element(
-                By.XPATH,
-                "//*[@id='search-result']/li[1]/div/div[2]/div/div/div[2]/div/div[1]/div[1]",
-            )
+            flesh_web = driver.find_element(By.XPATH,
+                                            "//*[@id='search-result']/li[1]/div/div[2]/div/div/div[2]/div/div[1]/div[1]")
             flesh = flesh_web.text[0]
             condition = condition_web.text.partition(flesh)[0]
 
         # Создаем элемент сегоднешняя дата
         date_now = datetime.date.today()
         with allure.step("Запись в файл"):
-            f = open("C://Users//Vitaliy//Desktop//Avtotest//weather.txt", "w")
-            f.write("Погода сейчаc: " + weather.text + " " + condition.rstrip() + "\n")
+            f = open("weather.txt", "w")
+            f.write('Погода сейчаc: ' + weather.text + ' ' + condition.rstrip() + "\n")
 
-        print("Погода сейчаc:", weather.text, condition.rstrip())
+        print('Погода сейчаc:', weather.text, condition.rstrip())
         # Создаем переменную для увеличение даты погоды
         day_count = 0
         with allure.step("Цикл погоды"):
@@ -68,32 +65,15 @@ class TestPageSearch:
                 tomorrow = date_now + datetime.timedelta(days=day_count)
 
                 with allure.step("Погда на день/ ночь"):
-                    day = driver.find_element(
-                        By.XPATH,
-                        "//*[@id='search-result']/li[1]/div/div[3]/div/div["
-                        + str(day_count + 1)
-                        + "]/div[2]",
-                    )
-                    niht = driver.find_element(
-                        By.XPATH,
-                        "//*[@id='search-result']/li[1]/div/div[3]/div/div["
-                        + str(day_count + 1)
-                        + "]/div[4]",
-                    )
+                    day = driver.find_element(By.XPATH,
+                                          "//*[@id='search-result']/li[1]/div/div[3]/div/div[" + str(day_count + 1) + "]/div[2]")
+                    niht = driver.find_element(By.XPATH,
+                                           "//*[@id='search-result']/li[1]/div/div[3]/div/div[" + str(day_count + 1) + "]/div[4]")
+
 
                 with allure.step("Запись в файл в цикле"):
-                    print(tomorrow, "день:", day.text, "ночь:", niht.text)
-                    f.write(
-                        str(tomorrow)
-                        + " день:"
-                        + day.text
-                        + " ночь:"
-                        + niht.text
-                        + "\n"
-                    )
-
-            print(day_count)
+                    print(tomorrow, 'день:', day.text, 'ночь:', niht.text)
+                    f.write(str(tomorrow) + ' день:' + day.text + ' ночь:' + niht.text + "\n")
 
         f.close()
-        with allure.step("Скриншот"):
-            driver.save_screenshot("C://Users//Vitaliy//Desktop//Avtotest//weather.png")
+        driver.save_screenshot("weather.png")
